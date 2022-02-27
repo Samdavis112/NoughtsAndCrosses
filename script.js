7cd2ceb88d048f1b208f03f2bfab8d657288d3f0
Samdavis112 <93987384+Samdavis112@users.noreply.github.com>
2022-02-27 14:06:32 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+let XTurn = true;
+
+let gameBoard = [
+    [],
+    [],
+    []
+];
+
+function getCellData(cellNumber){
+    const cellData = document.getElementById(cellNumber).innerHTML;
+    return cellData;
+}
+
+function addCellClickEvent(cellNumber){
+    document.getElementById(cellNumber).addEventListener('click', function(){
+        onClick();
+    })
+}
+
+function setCellData(cellNumber, cellData){
+    document.getElementById(cellNumber).innerHTML = cellData;
+}
+
+function getBoard(){
+    const board = [
+        [getCellData(0), getCellData(1), getCellData(2)],
+        [getCellData(3), getCellData(4), getCellData(5)],
+        [getCellData(6), getCellData(7), getCellData(8)]
+    ]
+    return board;
+}
+
+function setBoard(gameBoard){  
+    setCellData(0, gameBoard[0][0]);
+    setCellData(1, gameBoard[0][1]);
+    setCellData(2, gameBoard[0][2]);
+    setCellData(3, gameBoard[1][0]);
+    setCellData(4, gameBoard[1][1]);
+    setCellData(5, gameBoard[1][2]);
+    setCellData(6, gameBoard[2][0]);
+    setCellData(7, gameBoard[2][1]);
+    setCellData(8, gameBoard[2][2]);
+}
+
+function setCellColour(cellNumber, colour){
+    if(colour == "colour-white"){
+        document.getElementById(cellNumber).classList.remove("colour-black");
+    }
+    else{
+        document.getElementById(cellNumber).classList.remove("colour-white");
+    }
+    document.getElementById(cellNumber).classList.add(colour);
+}
+
+function startGame(){
+    //Setting font colour to black
+    for(i = 0; i < 9; i++){
+        setCellColour(i, "colour-black")
+    }
+}
+
+function main(){
+
+    //formatting cells 
+    startGame();
+    
+    //adding cell click event
+    for(i = 0; i < 9; i++){
+        addCellClickEvent(i)
+    }
+}
+
+function onClick(){
+
+    let clickedElement = event.target.id;
+    let cellNumber = clickedElement;
+
+    if(XTurn == true){
+        XTurn = false;
+        setCellColour(cellNumber , "colour-white");
+        setCellData(cellNumber , "X");
+    }
+    else{
+        XTurn = true;
+        setCellColour(cellNumber, "colour-white");
+        setCellData(cellNumber , "O");
+    }
+}
+
+
+
+
+
+
+
+
+
+main();
